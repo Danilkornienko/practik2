01ef71ee499b335f4d880bdd3aa58eeb1111e2ff
Danilkornienko <144102878+Danilkornienko@users.noreply.github.com>
2024-06-05 22:48:39 +0300

    v1

---
+++ app/src/main/java/ru/rsue/kornienko/bookdepository/MainActivity.java
@@ -0,0 +1,23 @@
+package ru.rsue.kornienko.bookdepository;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null){
+            fragment = new BookFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+    }
+}}
